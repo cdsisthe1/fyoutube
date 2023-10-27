b06bf442db41cdc411a9f4c6e356b51b4d020152
cdsisthe1 <86140616+cdsisthe1@users.noreply.github.com>
2023-10-27 13:23:02 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -10,9 +10,38 @@ document.getElementById('fetch_link').addEventListener('click', function() {
     .then(response => response.json())
     .then(data => {
         if(data.direct_link) {
-            document.getElementById('result').textContent = data.direct_link;
+            const resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = `<a href="#" id="copied_link">${data.direct_link}</a>`;
+            
+            // Add event listener for the new link
+            document.getElementById('copied_link').addEventListener('click', function(event) {
+                event.preventDefault();
+                copyToClipboard(data.direct_link);
+                resultDiv.textContent = "copied url";
+            });
         } else {
             document.getElementById('result').textContent = data.error;
         }
     });
 });
+
+document.getElementById('github_link').addEventListener('click', function() {
+    chrome.tabs.create({ url: 'https://github.com/cdsisthe1/fyoutube' });
+});
+
+document.getElementById('donate_link').addEventListener('click', function() {
+    chrome.tabs.create({ url: 'https://www.paypal.com/donate/?hosted_button_id=R92KGPYHPE3JY' });
+});
+
+
+
+function copyToClipboard(text) {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+}
+
+
